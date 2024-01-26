0a7898e9b5101fdd18483aa57031722daca067ad
Melina <mtidblad@kth.se>
2024-01-26 18:24:24 +0100

    alla spel tillagda! Placering av spel är i rader anpassade till fönstrets storlek

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -3,7 +3,30 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import logo from './logo.svg';
 import kvackman from './Kvackman.png';
+import fnollsflykt from './fnolls_flykt.png';
+import codenames from './codenames.png';
+import campus_d from './campus_defence.png';
+import te1ris from './te1ris.png';
 import './App.css';
+
+
+
+function Game({ link, name, img_name}) {
+  return (
+    <a
+      className="App-link"
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ display: 'inline-block', margin: '0.5%' }}
+    >
+      <h2>{name}</h2>
+      <img src={img_name} width={150} height={150} alt='Här är en bild på spelet'/>
+      
+    </a>
+  );
+}
+
 
 function App() {
   return (
@@ -12,18 +35,15 @@ function App() {
       <p>
         Hej och Välkomna samtliga Fysematiker till denna plats. Här hittar ni Fysiksektionens legendariska spel!
       </p>
-        <a
-          className="App-link"
-          href="https://f.kth.se/arcade/kvackman"
-          target="_blank"
-          rel="noopener noreferrer"
-        > 
-          <h2 className="kvackman-text">Kvackman</h2>
-        </a>
-        <img src={kvackman} width={200} height={200} style={{ position: 'relative', top: '10px', left: '-200px'}}/>
-
+      <div className="games-container">
+        <Game link="https://f.kth.se/arcade/kvackman" name="Kvackman" img_name={kvackman}/>
+        <Game link="https://f.kth.se/arcade/fnollsflykt/" name="Fnolls Flykt" img_name={fnollsflykt}/>
+        <Game link="https://f.kth.se/arcade/codenames/" name="Codenames" img_name={codenames}/>
+        <Game link="https://f.kth.se/arcade/campusdefence" name="Campus Defence" img_name={campus_d}/>
+        <Game link="https://pyro-cryo.github.io/Te1ris/" name="Te1ris" img_name={te1ris}/>
+        {/* Lägg till fler spel här */}
+      </div>
     </div>
-
   );
 }
 
